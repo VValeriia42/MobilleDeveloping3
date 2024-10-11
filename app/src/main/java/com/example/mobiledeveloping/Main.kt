a2038e0e01582b4cba04e4ac32d2f639b2e4e21e
VValeriia42 <valeriakolaskina139@gmail.com>
2024-10-11 09:58:00 +0300

    лабораторная1 без 5 задания

---
+++ app/src/main/java/com/example/mobiledeveloping/Main.kt
@@ -0,0 +1,38 @@
+package com.example.mobiledeveloping
+
+fun main() {
+    //println("*" )
+   // println("**")
+   // println("***" )
+   // println("****" )
+   // println("*****")
+   // val number1 = arrayOf(10,1,6,6,7)
+    //val int = number1.toList()
+   // val a=int.min()
+    //val b=int.max()
+   // val c=b-a
+   // println("Максимальный элемент $b")
+    //println("Минимальный элемент$a")
+    //println("Разница $c")
+   // val number1=10
+   // val number2 = 22
+   // val number3 =50
+
+    //when (number1 )
+    //{
+     //   110 -> println("hh")
+     //   else -> println("10")
+   // }
+   // when (number2 )
+   // {
+    //    25 -> println("hh")
+    //    else -> println("22")
+   // }
+    //when (number3 )
+    //{
+     //   501 -> println("hh")
+     //   else -> println("50")
+    //}
+   // println("Наибольшее число $number3")
+
+}
